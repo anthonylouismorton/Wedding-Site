243979d3238f7e598c706a59973223a5f4f6f609
Anthony Louis Morton <anthonymorton760@gmail.com>
2021-09-17 08:18:19 -0700

    converted to React

---
+++ src/Components/Header.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default class Header extends Component {
+  render(){
+    return(
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Gallery</Nav.Link>
+          <Nav.Link href="#features">Venue</Nav.Link>
+          <Nav.Link href="#pricing">Registry</Nav.Link>
+          <Nav.Link href="#pricing">Wedding Photos</Nav.Link>
+          <Nav.Link href="#pricing">GuestBook</Nav.Link>
+          <Nav.Link href="#pricing">RSVP</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+    )
+  }
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import Container from 'react-bootstrap/Container'
+
+export default class Main extends Component{
+  render(){
+    return (
+      <Container>
+        <h1>I am the main</h1>
+      </Container>
+    )
+  }
+}
