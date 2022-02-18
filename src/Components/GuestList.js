e3f3e69fb2dde6aed799379359269d7be9c38ea5
Anthony Louis Morton <anthonymorton760@gmail.com>
2022-02-18 12:31:49 -0800

    fixed more netlify errors

--- src/Components/GuestList.js
+++ src/Components/GuestList.js
@@ -38,7 +38,7 @@ const classes = useStyles();
 const [order, setOrder] = useState('asc');
 const [orderBy, setOrderBy] = useState('name');
 const [page, setPage] = useState(0);
-const [dense, setDense] = useState(false);
+// const [dense, setDense] = useState(false);
 const [rowsPerPage, setRowsPerPage] = useState(5);
 
 const handleOpen = async () => {
